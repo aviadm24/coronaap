2925169641799b843ed33a3c3cfae43e26658c82
yosef moshe <yosef moshe>
2020-03-25 11:03:25 +0200

    statis like heroku docs

--- korona_crm/settings.py
+++ korona_crm/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 print('project root: ', PROJECT_ROOT)
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# print('STATIC_ROOT: ', STATIC_ROOT)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, "static"),
+# )
+# STATIC_URL = '/static/'
+print('project root: ', BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 print('STATIC_ROOT: ', STATIC_ROOT)
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
-)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
