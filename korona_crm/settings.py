c79dc08e752721faa679598e980b50afcfde8dcf
yosef moshe <yosef moshe>
2020-04-01 17:37:51 +0300

    change static file dirs

--- korona_crm/settings.py
+++ korona_crm/settings.py
@@ -129,6 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 if ipaddress.startswith('172'):
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'staticfiles'),
     )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
