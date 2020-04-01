d8d1530b352200243fa889b48d0bbf61bbd18907
yosef moshe <yosef moshe>
2020-04-01 17:15:16 +0300

    added admin file

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User_tokens, Feedback, Time
 
-# Register your models here.
+admin.site.register(User_tokens)
+admin.site.register(Feedback)
+admin.site.register(Time)
