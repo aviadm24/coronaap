4a7a403b81b8dc7360ecd50667afd3271fddfff0
Jonathan Aharon <jonathan.aharon@perception-point.io>
2020-04-10 16:26:29 +0300

    bugfix in main/urls.py - updated url names

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('update_sheets', views.update_sheets, name='update_sheets'),
-    path('aviad_sheets', views.aviad_sheets, name='aviad_sheets'),
-    path('create_sheet', views.create_sheet, name='create_sheet'),
+    path('incoming_sms', views.incoming_sms, name='incoming_sms'),
     path('send_sms', views.send_sms, name='send_sms'),
     path('cancel_sms', views.cancel_sms, name='cancel_sms')
 ]
